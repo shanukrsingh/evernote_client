3f140bfe3eed0ba5578ea87cc596a12b1ddbd67b
shanukrsingh <56463680+shanukrsingh@users.noreply.github.com>
2022-08-15 17:35:15 +0530

    Update apiEndpoints.js

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://evernotecloneserver.herokuapp.com/';
+export const BASE_URL = 'https://noteappserver.herokuapp.com';
 export const CREATE_NOTE = '/api/note';
 export const UPDATE_NOTE = '/api/note/';
 export const DELETE_NOTE = '/api/note/';
