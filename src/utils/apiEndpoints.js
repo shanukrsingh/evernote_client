00b62d9c622482e348eba4b64e2f02f6f54f8a07
shanukrsingh <shanukrsinghhis@gmail.com>
2022-08-15 13:17:35 +0530

    v3

---
+++ src/utils/apiEndpoints.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://evernotecloneserver.herokuapp.com/';
+export const CREATE_NOTE = '/api/note';
+export const UPDATE_NOTE = '/api/note/';
+export const DELETE_NOTE = '/api/note/';
+export const GET_ALL_NOTES = '/api/notes/all';
+export const GET_TRASH_NOTES = '/api/notes/trash';
